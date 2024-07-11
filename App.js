5738debacb7fef298f73c7de7d38ff6f6ffa8577
Skylar-Lorena <lorayne911@gmail.com>
2024-07-11 12:29:00 +0300

    chore: :fire: cleanup of main and wrap with nav container

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StyleSheet, View } from 'react-native';
+import LoveCalculator from './components/LoveCalculator';
 import WelcomeScreen from './screens/WelcomeScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <WelcomeScreen/>
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName= "Welcome">
+          <Stack.Screen name="Welcome" component={WelcomeScreen}/>
+          <Stack.Screen name="LoveCalculator" component={LoveCalculator}/>
+     </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
